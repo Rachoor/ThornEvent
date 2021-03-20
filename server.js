598477c282939ec702825ccd74b2911258b6e631
Phalguna Kumar Rachoor <ethanhunt@Phalgunas-MacBook-Pro.local>
2021-03-20 10:35:00 +0530

    updated server.js and package.json files for deployment

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
-const app = express();
-app.use(express.static(__dirname+'/dist/eventbrite'));
-const forceSSL = function() {
-    return function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-         ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
-    }
-  }
 
-app.use(forceSSL());
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/eventbrite'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/eventbrite/index.html'));
+    
+res.sendFile(path.join(__dirname+'/dist/eventbrite/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
